ac5c6683215ce10b93f9e5159e4d507dc3e0f1a3
Sahil-Iqubal <sahiliqubal06@gmail.com>
2025-04-20 23:03:16 +0530

    Connected database and added utils for error and other dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello developers");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
